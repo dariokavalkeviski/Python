e1ec6de0a23e17838f76b720c00fde2fffbd51a4
Dario Kavalkeviski <sdpmdario@hotmail.com>
2022-01-19 21:55:17 -0300

    versao inicial

---
+++ estoque.py
@@ -0,0 +1,18 @@
+#estoque.py
+
+
+produto = input('Digite o nome do produto:')
+preco = float(input('Digite o preco:'))
+quantidade = int(input('Digite a quantidade: '))
+total = quantidade * preco
+
+#informar produto, prço e quantidade
+print('Nome do produto:' , produto)
+print('Preco do produto R$', preco)
+print('Quantidade do produto:', quantidade)
+print('Total do estoque R$ %.2f' % total)
+
+
+
+      
+
---
+++ login.py
@@ -0,0 +1,12 @@
+#login.py
+
+nome = input("Digite seu nome:")
+senha = input("Digite a senha:")
+              
+if (senha == "mestra"):
+    print("Bem vindo " + nome + " ao sistema")
+else:
+    print("Senha invalida")
+
+    
+    
