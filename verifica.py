283b9063aa8cbde31b95fa4022f2659976acc370
Dario Kavalkeviski <86028029+dariokavalkeviski@users.noreply.github.com>
2022-02-17 21:11:36 -0300

    Update verifica.py

--- verifica.py
+++ verifica.py
@@ -1,10 +1,10 @@
-#verifica.py
+# Verifica.py
 
-#Algoritimo
-#1.cria uma lista de cores
-#2.pergunta ao usuario o nome de uma cor
-#3.diz para o usuario se a cor existe na lista
-#4.Se nao ta na lista, adiciona (anexar = append) na lista
+# Algoritimo
+# 1.cria uma lista de cores
+# 2.pergunta ao usuario o nome de uma cor
+# 3.diz para o usuario se a cor existe na lista
+# 4.Se nao ta na lista, adiciona (anexar = append) na lista
 
 cores = ['verde', 'amarelo', 'azul', 'branco', 'preto', 'branco', 'vermelho', 'verde', 'azul', 'amarelo']
 cor = input('digite nome de uma cor:')
